a38759090eed5c45f9d7aaae049b138fc7374485
Thomas Rafferty & Chris Camp <pair+ThomasRafferty.cpcamp2@devbootcamp.com>
2017-08-30 16:30:19 -0400

    Release 0 & 1 complete

--- linear_search.rb
+++ linear_search.rb
@@ -9,3 +9,20 @@ def linear_search(target, array)
   end
 end
 
+def global_linear_search(target, array)
+  i = 0
+  found_array = []
+  until i > array.length
+    if array[i] == target
+       found_array << i
+       i += 1
+    else
+      i += 1
+    end
+  end
+  if found_array.length == 0
+    return nil
+  else
+    return found_array
+  end
+end
--- spec/linear_search_spec.rb
+++ spec/linear_search_spec.rb
@@ -20,5 +20,19 @@ end
 
 
 describe 'global_linear_search' do
+  let(:symbols) { [:Q, :O, :A, :A, :C, :G, :S, :U] }
 
+  context 'when the array contains the searched-for element' do
+    it 'returns an array of indices of occurrences of the element' do
+      index_of_a = global_linear_search(:A, symbols)
+      expect(index_of_a).to eq [2, 3]
+    end
+  end
+
+  context 'when the array does not contain the searched-for element' do
+    it 'returns nil if the element cannot be found' do
+      index_of_t = global_linear_search(:T, symbols)
+      expect(index_of_t).to eq nil
+    end
+  end
 end
