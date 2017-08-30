22309b3b25653d7e82aa3272fd101ced7fb26f7e
Catherine Rice & David Skaggs <pair+clr2107.davidtskaggs@devbootcamp.com>
2017-08-30 17:27:42 -0400

    Add global linear search

--- linear_search.rb
+++ linear_search.rb
@@ -9,3 +9,14 @@ until counter == array.length
 return nil
 end
 
+def global_linear_search(element, data_str)
+  index_array = []
+  counter = 0
+  until counter == data_str.length
+    if data_str[counter] == element
+      index_array << counter
+    end
+    counter += 1
+  end
+  p index_array
+end
